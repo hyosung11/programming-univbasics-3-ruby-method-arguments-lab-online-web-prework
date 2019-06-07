fce24d0dc6ff81e617a570a72eb5fec6e376c0a0
HyoSung Bidol-Lee <hyosung11@me.com>
2019-06-07 19:11:54 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -13,8 +13,3 @@ def introduction_with_language_optional(name, language = "Ruby")
 end
 introduction_with_language_optional("Sohee")
 
-def introduction_with_language_optional(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end
-
-introduction_with_language_optional("HyoSung" "JavaScript")
